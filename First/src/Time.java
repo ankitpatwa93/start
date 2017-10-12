844a2de06f4a2b186bd93f3fba135afeecad1a6c
ankit.patwa <ankit.patwa@YPTEMP02LT.Yash.com>
2017-10-12 12:47:53 +0530

    new class file time

---
+++ First/src/Time.java
@@ -0,0 +1,29 @@
+
+interface One
+{
+	public static int id=1;
+}
+
+interface Two
+{
+	public static int id=1;
+}
+
+public class Time implements One,Two
+{
+
+	Integer name;
+	String time;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Yes id==="+One.id);
+		System.out.println(new Time().name+" "+new Time().time+"address==");
+	}
+	
+	public static void instanceMethod(){
+			
+		 String  address;
+	}
+
+}
