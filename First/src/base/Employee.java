37e33feee2bdff4f05d06496f031dd111aa1ab0b
vivek.mishra <vivek.mishra@YP1007005LT.Yash.com>
2017-10-12 17:49:41 +0530

    done by vivek

--- First/src/base/Employee.java
+++ First/src/base/Employee.java
@@ -5,7 +5,15 @@ public class Employee {
 	int id;
 	private String name;
 	
-     
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+     public void setName(String name){
+    	 this.name=  name;
+     }
 	
 
 	public int getId() {
